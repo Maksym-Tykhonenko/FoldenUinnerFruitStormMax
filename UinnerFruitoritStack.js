8019013f9028b4dcb4468a161614af4978e17a34
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-12-10 16:21:00 +0200

    ccc

--- UinnerFruitoritStack.js
+++ UinnerFruitoritStack.js
@@ -342,7 +342,7 @@ const ForgeRoutes = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-12-01T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-12-12T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
